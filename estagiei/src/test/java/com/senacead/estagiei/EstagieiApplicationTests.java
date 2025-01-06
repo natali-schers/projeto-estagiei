7ac5341b884514f6126c9054abbd64aced209e87
Natali Schers <schersnatali@gmail.com>
2025-01-05 23:44:52 -0300

    Add project files.

---
+++ estagiei/src/test/java/com/senacead/estagiei/EstagieiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senacead.estagiei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstagieiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
