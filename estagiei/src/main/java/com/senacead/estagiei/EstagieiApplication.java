f8bf3fcc533963cf4a2c83aa7cc87e0d3285fb71
Natali Schers <schersnatali@gmail.com>
2025-01-05 23:44:52 -0300

    Add project files.

---
+++ estagiei/src/main/java/com/senacead/estagiei/EstagieiApplication.java
@@ -0,0 +1,13 @@
+package com.senacead.estagiei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstagieiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstagieiApplication.class, args);
+	}
+
+}
